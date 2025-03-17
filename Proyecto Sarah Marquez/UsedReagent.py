bc59060af720f6f3591fa322d44e87190174adc8
sarahmarquezf <sarah.marquez@correo.unimet.edu.ve>
2025-03-16 23:50:49 -0400

    Delete Proyecto Sarah Marquez directory

--- Proyecto Sarah Marquez/UsedReagent.py
+++
@@ -1,5 +0,0 @@
-class UsedReagent:
-    def __init__(self, quantity, id, unit):
-        self.quantity = quantity
-        self.id = id
-        self.unit = unit
--- Proyecto Sarah Marquez/Values.py
+++
@@ -1,6 +0,0 @@
-class Value:
-    def __init__(self, name, formula, minimum, maximum):
-        self.name = name
-        self.formula = formula
-        self.minimum = minimum
-        self.maximum = maximum
